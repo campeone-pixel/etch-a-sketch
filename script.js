3112ddd674797dc77fc86138f47febce1420d665
campeone-pixel <posesmatiasabel@gmail.com>
2022-05-13 22:17:36 -0300

    i found the way to make the grid bigger or smaller

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 const containerGrid = document.querySelector("body > section > div.container-grid > div");
 
+const heightSquares = 50
 let fragment = document.createDocumentFragment();
 
- for(let x = 0;x < 16   ; x++){
-    for(let i = 0;i < 16   ; i++){
+ for(let x = 0;x < heightSquares   ; x++){
+    for(let i = 0;i < heightSquares   ; i++){
       let littlePixel = document.createElement("div");
       littlePixel.setAttribute("class","pixel")
+      littlePixel.setAttribute("style",`height:${100/heightSquares}%`)
       /* littlePixel.textContent="1" */
       fragment.appendChild(littlePixel);
   }
@@ -13,4 +15,14 @@ let fragment = document.createDocumentFragment();
 }
 
 containerGrid.appendChild(fragment)
-console.log(fragment)
+
+const pixeles = document.querySelectorAll("body > section > div.container-grid > div>div")
+
+function pintarEnNegro(div){
+  div.style="color-background: black"
+}
+
+function pintarRandom(div){
+
+  div.style="color-background: rgb"
+}
